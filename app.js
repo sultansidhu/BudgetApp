21adec64689f713f718f6135724e044dad4f50c2
sultansidhu <sultansinghsidhu@gmail.com>
2018-11-07 14:46:37 -0500

    added some functions

--- app.js
+++ app.js
@@ -10,8 +10,62 @@ Created by learning from a Udemy Course by Jonas Schmedtmann
 
 // BUDGET CONTROLLER
 var budgetController = (function(){
-    // code
-});
+    var Expense = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    var Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+    var allExpenses = [];
+    var allIncomes = [];
+    var totalExpenses = 0;
+    
+    var data = {
+        allItems:{
+            exp: [],
+            inc: []
+        },
+        totals:{
+        exp: 0,
+        inc: 0
+    }
+}
+    return {
+        addItem: function(type, des, val){
+            var newItem, ID;
+            //creating a new ID
+            console.log("THE LENGTH BEFORE ADDING ANYTHING IS " + data.allItems[type].length);
+            if (data.allItems[type].length > 0){
+                ID = data.allItems[type][data.allItems[type].length-1]+1;
+            } else {
+                 ID = 0;
+            }
+           
+            //checking if the new addition is an expense or an income 
+            console.log('TYPE GIVEN IN IS' + type);
+            if (type==="exp"){
+                newItem = new Expense(ID, des, val);
+            } else if (type==="inc"){
+                newItem = new Income(ID, des, val);
+            }
+            
+            //pushing it onto the data structure
+            console.log("THE NEW ITEM BEING ADDED IS :");
+            console.log(newItem);
+            data.allItems[type].push(newItem);
+            console.log('NOW THE LENGTH OF THE LIST IS ' + data.allItems[type].length);
+            return newItem
+        }, 
+        testing : function(){
+        console.log(data)
+    }
+        
+    }
+                        });
 
 
 // UI CONTROLLER
@@ -62,11 +116,11 @@ var Controller = (function(budgetController, UICtrl){
         
     var ctrlAddItem = function(){
         // get field item data
-        var input = UICtrl.getInput();
+        var input = UICtrl().getInput();
         console.log(input);
         
         // add data to the budget controller
-        
+        var newItem = budgetController().addItem(input.type, input.description, input.value);
         
         // add data to the ui controller
         
